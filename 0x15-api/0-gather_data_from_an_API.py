cf0ec19f861a42f691e0b67fed89669402b73275
b <bruno932151@gmail.com>
2022-05-16 18:01:14 -0700

    we comsume jsonapi with librery request

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,6 +2,26 @@
 """
 Python script that, using this REST API
 """
+import requests
+from sys import argv
 
 
+def gather():
+    """ get date from api """
+    url = "https://jsonplaceholder.typicode.com"
+    user = requests.get("{}/users?id={}".format(url, argv[1])).json()[0]
+    todos = requests.get("{}/todos?userId={}".format(url, argv[1])).json()
+    name = user.get("name")
+    todok = 0
+    prompt = "Employee {} is done with tasks({}/{}):"
+    for todo in todos:
+        if todo.get('completed'):
+            todok = todok + 1
+    print(prompt.format(name, todok, len(todos)))
+    for todo in todos:
+        if todo.get('completed'):
+            print("\t {}".format(todo.get('title')))
 
+
+if __name__ == "__main__":
+    gather()
