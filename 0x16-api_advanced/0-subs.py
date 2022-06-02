6769b2fffc1829377c7a59d3b024b5875718c7b2
BrunoGS <bruno932151@gmail.com>
2022-06-01 19:32:55 -0500

    task0 add

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+that queries the Reddit API and
+returns the number of subscribers
+"""
+import requests
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+          AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    resp =  requests.get(url, headers=header)
+    if resp.status_code == 200:
+        data = resp.json().get("data").get("subscribers")
+        return(data)
+    return (0)
